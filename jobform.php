388cfaabdaa0d2dba0db876185aa3d20dc91cdc1
P@project99 <74760933+censorday@users.noreply.github.com>
2021-09-15 22:10:08 +0530

    cid

---
+++ jobform.php
@@ -0,0 +1,20 @@
+<?php
+
+
+$servername = "sql6.freesqldatabase.com";
+$username = "sql6437316";
+$password = "r94vS6MuAu";
+$dbname = "sql6437316";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+//check
+  if($conn === false){
+            die("ERROR: Could not connect. " 
+                . mysqli_connect_error());
+        }
+
+        $compid =  $_REQUEST['cid'];
+
+        echo "output:  $compid. " ;
