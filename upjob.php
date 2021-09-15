35800294be6c98b79bf8f89c05458692e173ccd6
P@project99 <74760933+censorday@users.noreply.github.com>
2021-09-15 19:17:46 +0530

    Update upjob.php

--- upjob.php
+++ upjob.php
@@ -8,9 +8,26 @@ $dbname = "sql6437316";
 
 // Create connection
 $connect = new mysqli($servername, $username, $password, $dbname);
+if (!$connect)
 
+  {
+
+  die('Could not connect: ' . mysql_error());
+
+  }
 
 
 mysqli_query($connect,"INSERT INTO comp (company, post, salary, city , type ,  date)
 VALUES ('$_POST[cpname]', '$_POST[pst]', '$_POST[sal]','$_POST[loc]','$_POST[catg]',  '$_POST[pblh]')";
 ?>
+<html>
+<body>
+
+<center><h1>Update in progress .. .. ..</h1>
+<br>
+<p><b>please wait, it will automaticly redirect </b></p>
+
+</center>
+
+</body>
+  </html>
