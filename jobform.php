9d55001a39b2b0bb4b3169de21a9d206efae7312
P@project99 <74760933+censorday@users.noreply.github.com>
2021-09-15 23:09:11 +0530

    inp

--- jobform.php
+++ jobform.php
@@ -21,3 +21,202 @@ $conn = new mysqli($servername, $username, $password, $dbname);
         $ct=$_GET['ct'];
         $tp=$_GET['tp'];
         $dt=$_GET['dt'];
+
+
+        <html>
+        <style>
+        
+        
+        @import url('https://fonts.googleapis.com/css2?family=Karla:wght@200&display=swap');
+        
+        
+        body{
+        background-color: #e8e8e8;
+        font-family: 'Karla', sans-serif;
+        
+        }
+        
+        .back{
+        background-color: #2a2a72;
+        background-image: linear-gradient(115deg, #2a2a72 0%, #009ffd 74%);
+        height:500px;
+        color:white;
+        overflow:hidden;
+        }
+        
+        .title{
+          font-family:arial;
+          font-size:40px;
+          font-weight:80;
+          letter-spacing:5px;    
+          
+        }
+        
+        .box{
+        
+        padding:20px 10px;
+        margin-top:-240px;
+        margin-bottom:25px;
+        background-color: white;
+        box-shadow:5px 7px 12px black;
+        border-radius: 1%;
+        max-width:85%;
+        min-height:450px;
+        text-align:center;
+        color:black;
+        overflow:hidden;
+        }
+        table {
+        
+          border: 1px solid #ccc;
+          color:black;
+          border-collapse: collapse;
+          margin: 0;
+          padding: 0;
+          width: 75%;
+          table-layout: fixed;
+        }
+        
+        
+        
+        table tr {
+          background-color: #f8f8f8;
+          border: 5px solid white;
+        
+        }
+        
+        table th,
+        table td {
+          padding: 10px;
+          text-align: center;
+        }
+        
+        table th {
+          font-size: 16px;
+          letter-spacing: 1px;
+          text-transform: uppercase;
+        }
+        
+        .apply{
+           margin:2%;
+            font-size: 12px;
+            height: 35px ;
+            padding: 0px 10px;
+            border: none ;
+            border-radius:5px;
+           background: #157efb;
+           color: white;
+        }
+        .apply:hover{
+            background: #f5f5f5;
+          color:#157efb;
+        }
+          
+        .apply:active{
+          
+            background: #b97ebb;
+            color:white;
+        
+        }
+        .form1{
+          margin:2% 2%;
+          display: inline-block;
+          height: 32px;
+          padding: 6px 16px;
+          width: 35%;
+          border: none;
+          background-color: #f3f3f3;
+          }
+        
+        </style>
+        
+        <body>
+        <section class='back'>
+        <center><h1 class='title'>SUBMIT JOBS</h1>
+        <br>
+        <p><b>Here You can submit jobs.<br>so you can desired have your candidate</b></p>
+        
+        </center>
+         </section>  
+        
+        <center>
+        <section>
+        <div class='box' >
+        
+        <form action="upjob.php" method="post" >
+                <input	placeholder="First Name"
+             class="form1" type="text" name="firstname" >
+        
+           <input placeholder="Last Name"
+             class="form1" type="text" name="lastname" >
+          <input placeholder="Father's Name"
+             class="form1" type="text" name="fathername" >
+        
+           <input placeholder="Email"
+             class="form1" type="email" name="email" >
+        
+            <input placeholder="Perm. Address"
+            class="form1" type="address" name="address" >
+        
+          <input  placeholder="City"
+             class="form1" type="text" name="city" >
+        
+           <input  placeholder="Phone"
+            class="form1" type="tel" name="phone" >
+        
+          
+            
+        
+        
+             <input class="form1" placeholder="Date-of-Birth"  type="text" onfocus="(this.type='date')"   name="dob" >
+        
+        
+            <select name="qul" id="" class="form1">
+        <option value="">Primary Qualification</option>
+        <option value="B.tech">B.Tech</option>
+        <option value="M.Tech">M.Tech</option>
+        <option value="B.Sc">B.Sc</option>
+        <option value="M.Sc">M.Sc</option>
+        <option value="M.Ba">M.Ba</option>
+        <option value="12th">12th only</option>
+        <option value="10th">10th only</option>
+        </select>
+         
+        
+               <select name="catg" id="" class="form1">
+        <option value="">Job Prefrence</option>
+        <option value="Full-Time">Full Time</option>
+        <option value="Part-Time">Part Time</option>
+        <option value="Freelance">Freelance</option>
+        <option value="Internship">Internship</option>
+        <option value="Temporary">Temporary</option>
+        </select><br><br>
+        <small><b>Additional Qualification/Achivements: </b></small><br>
+           <textarea  placeholder=" "
+            class="form1"  name="dis" > </textarea>
+        <br>
+        <input type="checkbox"  name="wfh" value="wfh">  <small><b>I Prefer Work From Home </b></small></input>
+        <br><br>
+           <small><b>CV/Resume: </b></small>
+            <input class="form1" type="file" name="file" >
+                
+           
+                
+            <br>
+                <input type="submit" class="apply" value="Submit">
+          
+            </form>
+        
+        </div>
+        </section>
+        
+        <br>
+        <h1>Previous Job</h1>
+        <small><b>Recently submitted job will be shown below</b></small>
+        <br><br>
+        
+          </center>
+        
+        
+        </body>
+        </html>
